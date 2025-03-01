009477c930ad2d635e0fd54351ea529880909f53
Hurleveur <94366726+Hurleveur@users.noreply.github.com>
2025-03-01 16:52:56 +0100

    Update home.tsx

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -4,11 +4,14 @@ import EventDetail from "./event"
 
 export default function Home() {
   return (
-    <div className="h-full p-4">
+    <div className="h-full p-4"><h2 className="text-center text-2xl font-semibold mb-4">
+    Here you can find all the events happening at the university of Namur and around it, participate in them.<br></br>
+    You can also volonteer, in which case you'll win points you can use to buy free events later.
+  </h2>
       <div className="flex justify-center items-center">
         <img src="/unamur.jpg"></img>
         </div>
-        Here you can find all the events happening at the university of Namur and around it, participate in them. You can also volonteer, in which case you'll win points you can use to buy free events later.
+        
 
         <div className="min-h-screen bg-gray-100 flex justify-center items-center">
           <EventCarousel />
