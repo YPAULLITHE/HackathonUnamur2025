075d492279c7bc7ec6a4bddaf39eccf0001ad0de
Hurleveur <94366726+Hurleveur@users.noreply.github.com>
2025-03-01 12:15:43 +0100

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,17 +5,42 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: {
+          light: "#f3f4f6", // Light gray (gray-100)
+          dark: "#1f2937", // Dark gray (gray-900)
+        },
+        secondary: "#A7F3D0", // Light green (tailwind-green-300)
+        accent: "#FFFFFF", // White
+      },
+    },
   },
   plugins: [require("daisyui")],
   daisyui: {
     styled: true,
-    themes: ["cupcake", "dracula"],
+    themes: [
+      {
+        mytheme: {
+          "primary": "#4CAF50", // Green
+          "secondary": "#A7F3D0", // Light green
+          "accent": "#FFFFFF", // White
+          "neutral": "#1f2937", // Dark gray
+          "base-100": "#f3f4f6", // Light gray background
+          "info": "#2094f3",
+          "success": "#36d399",
+          "warning": "#fbbf24",
+          "error": "#f87272",
+        },
+      },
+      "cupcake", 
+      "dracula"
+    ],
     base: true,
     utils: true,
     logs: true,
     rtl: false,
     prefix: "",
-    darkTheme: "dracula",
+    darkTheme: "mytheme",
   },
-}
+};
